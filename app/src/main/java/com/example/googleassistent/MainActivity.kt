12dc7d6df71ab8260d6010d2bdc7a8977c3137ab
Suhani Ranka <suhanijainn006@gmail.com>
2025-03-23 11:09:58 +0530

    Initial commit: Add Android calculator app files

---
+++ app/src/main/java/com/example/googleassistent/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.googleassistent
+
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Mic
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.googleassistent.ui.theme.GoogleAssistentTheme
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+
+   lateinit var startForResult : ActivityResultLauncher<Intent>
+    var SpeakText  by
+        mutableStateOf("hi")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == RESULT_OK && it.data!=null){
+                var resultData = it.data
+                val resultArray = resultData?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+
+                SpeakText = resultArray?.get(0).toString()
+            }
+        }
+        setContent {
+            GoogleAssistentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SpeechToTextApp()
+                }
+            }
+        }
+    }
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun SpeechToTextApp() {
+
+    var context = LocalContext.current
+//    var SpeakText = remember {
+//        mutableStateOf("hi")
+//    }
+    Box (modifier = Modifier.fillMaxSize()){
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            IconButton(onClick = {
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak Now")
+                startForResult.launch(intent)
+
+            }) {
+                Icon(Icons.Rounded.Mic ,contentDescription = null)
+            }
+
+            Text(text = SpeakText)
+        }
+    }
+}
+
+
+
+}
